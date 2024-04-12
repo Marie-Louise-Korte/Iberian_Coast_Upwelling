57925f64825902c59ea968578d9125685572b894
Marie-Louise-Korte <ise.korte@gmx.de>
2024-04-12 16:58:21 +0100

    added loop for year

--- ERA5_download.py
+++ ERA5_download.py
@@ -2,53 +2,57 @@ import cdsapi
 
 c = cdsapi.Client()
 
-#MONTHS = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"] 
-MONTHS = ["01"]
+YEARS = np.array(np.arange(2021,2024,1), dtype = 'str')
+MONTHS = np.array(np.arange(1,13,1), dtype = 'str')
 
-for month in MONTHS: 
- 
-    result = c.service(
-        "tool.toolbox.orchestrator.workflow",
-         params={ 
-            "realm": "user-apps",
-            "project": "app-c3s-daily-era5-statistics",
-            "version": "master",
-            "kwargs": {
-                     "dataset": "reanalysis-era5-single-levels",
-                     "product_type": "reanalysis",
-                     "variable": "instantaneous_northward_turbulent_surface_stress",
-                     "statistic": "daily_mean",
-                     "year": "2020",
-                     "month": month,
-                     "time_zone": "UTC+00:00",
-                     "frequency": "1-hourly",
-                     "grid": "0.25/0.25",
-                     "area": {"lat": [35, 45], "lon": [-20, -5]} 
-              },
-              "workflow_name": "application"
-    })
-    c.download(result,[f"../Data.nosync/Surface_stress/Turbulent_instantaneous/N_2020_{month}.nc"])
 
-for month in MONTHS: 
- 
-    result = c.service(
-        "tool.toolbox.orchestrator.workflow",
-         params={ 
-            "realm": "user-apps",
-            "project": "app-c3s-daily-era5-statistics",
-            "version": "master",
-            "kwargs": {
-                     "dataset": "reanalysis-era5-single-levels",
-                     "product_type": "reanalysis",
-                     "variable": "instantaneous_eastward_turbulent_surface_stress",
-                     "statistic": "daily_mean",
-                     "year": "2020",
-                     "month": month,
-                     "time_zone": "UTC+00:00",
-                     "frequency": "1-hourly",
-                     "grid": "0.25/0.25",
-                     "area": {"lat": [35, 45], "lon": [-20, -5]}  
-              },
-              "workflow_name": "application"
-    })
-    c.download(result,[f"../Data.nosync/Surface_stress/Turbulent_instantaneous/E_2020_{month}.nc"])
+for year in YEARS:
+    for month in MONTHS: 
+     
+        result = c.service(
+            "tool.toolbox.orchestrator.workflow",
+             params={ 
+                "realm": "user-apps",
+                "project": "app-c3s-daily-era5-statistics",
+                "version": "master",
+                "kwargs": {
+                         "dataset": "reanalysis-era5-single-levels",
+                         "product_type": "reanalysis",
+                         "variable": "mean_northward_turbulent_surface_stress",
+                         "statistic": "daily_mean",
+                         "year": year,
+                         "month": month,
+                         "time_zone": "UTC+00:00",
+                         "frequency": "1-hourly",
+                         "grid": "0.25/0.25",
+                         "area": {"lat": [35, 45], "lon": [-20, -5]} 
+                  },
+                  "workflow_name": "application"
+        })
+        c.download(result,[f"../Data.nosync/Surface_stress/Turbulent_mean/N_{year}_{month}.nc"])
+
+
+for year in YEARS:
+    for month in MONTHS: 
+     
+        result = c.service(
+            "tool.toolbox.orchestrator.workflow",
+             params={ 
+                "realm": "user-apps",
+                "project": "app-c3s-daily-era5-statistics",
+                "version": "master",
+                "kwargs": {
+                         "dataset": "reanalysis-era5-single-levels",
+                         "product_type": "reanalysis",
+                         "variable": "mean_eastward_turbulent_surface_stress",
+                         "statistic": "daily_mean",
+                         "year": year,
+                         "month": month,
+                         "time_zone": "UTC+00:00",
+                         "frequency": "1-hourly",
+                         "grid": "0.25/0.25",
+                         "area": {"lat": [35, 45], "lon": [-20, -5]}  
+                  },
+                  "workflow_name": "application"
+        })
+        c.download(result,[f"../Data.nosync/Surface_stress/Turbulent_mean/E_{year}_{month}.nc"])
