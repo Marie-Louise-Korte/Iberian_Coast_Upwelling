2104c49216a53e04ef778c31aebe7d1370650575
Marie-Louise-Korte <ise.korte@gmx.de>
2024-05-02 17:10:24 +0100

    functions for summer and meridional mean, assign week index and month of week

--- my_functions.py
+++ my_functions.py
@@ -1,9 +1,14 @@
+## relevant packages
 import xarray as xr
 import numpy as np
 from global_land_mask import globe
 
-# write as function for calculating the upwelling Index
-def upwelling_index(DS, lat, lon, eastward_stress, northward_stress):
+
+##################################################################################################################
+## calculate the upwelling Index #################################################################################
+##################################################################################################################
+
+def calc_upwelling_index(DS, lat, lon, eastward_stress, northward_stress):
     # this function adds the new variables it creates to the input dataset DS
     
     ## Admin
@@ -57,15 +62,86 @@ def upwelling_index(DS, lat, lon, eastward_stress, northward_stress):
     theta = np.pi/2 + varphi_rad #? why this step?
 
     # -np.sin (changes the sign of the output) -> positive UI values mean upwelling favourable conditions
-    DS['upwelling_index'] = -np.sin(theta) * DS.ek_trans_u + np.cos(theta) * DS.ek_trans_v
-    DS.upwelling_index.attrs.update({"name" : "upwelling index", 
+    DS['UI'] = -np.sin(theta) * DS.ek_trans_u + np.cos(theta) * DS.ek_trans_v
+    DS.UI.attrs.update({"name" : "upwelling index", 
                                      "info" : "positive values upwelling favourable \nassuming Portuguese coast at 0°", 
                                      "ref" : "Gomez-Gesteira et al. 2006"})
     return DS
 
-def land_mask_add(DS):
+
+##################################################################################################################
+## add weekly index to my weekly resampled data & use index to assign month ######################################
+##################################################################################################################
+
+def add_week_and_month(DS_weekly_mean):
+    # creat an index for the weeks in the year, runs from 1 to 52 (53 some years) and then repeats
+    DS_weekly_mean['week_of_year'] = DS_weekly_mean.time.dt.isocalendar().week
+    DS_weekly_mean.week_of_year.attrs.update({'info' : 'index according to week of the year'})
+
+    # create a variable containing the assigned months of every week (has 53 weeks), the 53 week won't be used later and is therefore called 'Drop_53'
+    # this is following the information I got from Joaquim on someone else who used this to remove week 53 from CoRTAD data 
+    # the weeks are split up into months like this:
+        # Jan (1:4) - Feb (5:8) - Mar (9:13) 
+        # Apr (14:17) - May (18:21) - Jun (22:26) 
+        # Jul (27:30) - Aug (31:34) - Sep (35:39) 
+        # Oct (40:43) - Nov (44:47) - Dec (48:52)
+    months_list = ['Jan', 'Jan', 'Jan', 'Jan', 'Feb', 'Feb', 'Feb', 'Feb', 'Mar', 'Mar', 'Mar', 'Mar', 'Mar',
+     'Apr', 'Apr', 'Apr', 'Apr', 'May', 'May', 'May', 'May', 'Jun', 'Jun', 'Jun', 'Jun', 'Jun',
+     'Jul', 'Jul', 'Jul', 'Jul', 'Aug', 'Aug', 'Aug', 'Aug', 'Sep', 'Sep', 'Sep', 'Sep', 'Sep',
+     'Oct', 'Oct', 'Oct', 'Oct', 'Nov', 'Nov', 'Nov', 'Nov', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Drop_53']
+
+    # create an array the same length as my time coordinate and assign the months to the correct week in year indices
+    month_array = np.zeros(len(DS_weekly_mean.time))
+    month_array = month_array.astype(str)
+    
+    for i in np.arange(0, len(DS_weekly_mean.week_of_year)):
+        ind = DS_weekly_mean.week_of_year[i].values
+        month_array[i] = months_list[ind - 1]
+    
+    # assign as variable to the dataset -> just to have a reference for later
+    DS_weekly_mean['month'] = ('time', month_array)
+    DS_weekly_mean.month.attrs.update({'info' : 'gives the month each week in the dataset is assigned to'})
+    
+    return DS_weekly_mean
+
+##################################################################################################################
+# create a summer subset, if I want to change my subset I can change the min and max week ########################
+##################################################################################################################
+
+def subset_summer(DS_weekly_mean, min_week = 22, max_week = 39): 
+
+    DS_weekly_mean = add_week_and_month(DS_weekly_mean)
+    DS_new = DS_weekly_mean.where(((DS_weekly_mean.week_of_year >= min_week) & (DS_weekly_mean.week_of_year <= max_week)), drop = True)
+    
+    return DS_new
+
+
+##################################################################################################################
+# calculate the meridional mean ##################################################################################
+##################################################################################################################
+
+def calc_meridional_mean(DS, variable = 'UI', min_lat = 37.012264, max_lat = 43.844013, lon = -10):
+    
+    var = DS[f'{variable}']
+    
+    # default match the extent of the coast to UI_SST (min 37.012264, max 43.844013)
+    mean = var.sel(lon = lon, method = 'nearest').sel(lat = slice(min_lat, max_lat)).mean(dim = 'lat')
+    
+    DS[f'{variable}_mean'] = mean
+    DS[f'{variable}_mean'].attrs.update({'info' : f'meridional mean at 10°W (-10°) between {min_lat}°N and {max_lat}°N'})
+    
+    return DS
+    
+##################################################################################################################
+## calculate land mask -> has become a bit redundant -> I am using the ERA5 land-sea mask now ####################
+##################################################################################################################
+
+def add_land_mask(DS):
     lon_grid, lat_grid = np.meshgrid(DS.lon.values, DS.lat.values)
     land_mask = globe.is_land(lat_grid, lon_grid)                     # needs input with lat first then lon
     DS['land_mask'] = (('lat', 'lon'), land_mask)
     DS.land_mask.attrs.update({"name" : "land mask", "values" : "0 : land \n1 : ocean", "info" : "uses global_land_mask package"})  
     return DS
+
+
+
